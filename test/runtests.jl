81fe285ae9cd4c531c34df86c794500fb231b331
Jaan Tollander de Balsch <jaan.tollander@gmail.com>
2019-06-11 13:50:34 +0300

    MILP test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,9 @@ using JuMP, Gurobi
 
 function test_milp_gurobi()
     model = Model(with_optimizer(Gurobi.Optimizer))
-    @variable(model, 0 ≤ x ≤ 1)
-    @variable(model, 0 ≤ y ≤ 1)
-    @variable(model, 0 ≤ z ≤ 1)
+    @variable(model, x, Bin)
+    @variable(model, y, Bin)
+    @variable(model, z, Bin)
     @objective(model, Max, x + y + 2z)
     @constraint(model, con1, x + 2y + 3z ≤ 4)
     @constraint(model, con2, x + y ≥ 1)
