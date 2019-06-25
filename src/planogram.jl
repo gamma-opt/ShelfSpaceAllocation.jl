d56593634b7a068d20c6634709efe2e15d5c56ed
Jaan Tollander de Balsch <jaan.tollander@gmail.com>
2019-06-25 15:02:38 +0300

    Plot stackable product

--- src/planogram.jl
+++ src/planogram.jl
@@ -10,31 +10,37 @@ function planogram(products, shelves, blocks, P_b, H_s, W_p, W_s, n_ps, o_s, b_b
 
     # Draw products
     rect(x, y, w, h) = Shape(x .+ [0,w,w,0], y .+ [0,0,h,h])
+    # TODO: disctinct unique colors for all blocks
     block_colors = cgrad(:viridis)
+    # TODO: block labels
     # TODO: product labels
-    # TODO: product stack
-    # TODO: empty space
+    # TODO: disribute empty space equally
     for b in blocks
         for s in shelves
             x = x_bs[b, s]
             for p in P_b[b]
+                stack = max(min(div(H_s[s], H_p[p]), product_data.max_stack[p]), 1)
                 for i in 1:n_ps[p, s]
-                    plot!(plt, rect(x, y_s[s], W_p[p], H_p[p]),
-                          color=block_colors[b])
+                    y = 0
+                    for j in 1:stack
+                        plot!(plt, rect(x, y_s[s]+y, W_p[p], H_p[p]),
+                              color=block_colors[b/length(blocks)])
+                        y += W_p[p]
+                    end
                     x += W_p[p]
                 end
             end
         end
     end
 
-    # Draw shelves
+    # Draw shelves - A line from (0, y[s]) to (W_s[s], y[s])
+    # TODO: shelf labels
     for s in shelves
-        # Draw a line from (0, y[s]) to (W_s[s], y[s])
         plot!(plt, [0, W_s[s]], [y_s[s], y_s[s]],
-              color = :black, label="s_$s")
+              color=:black, label="s_$s")
     end
     plot!(plt, [0, W_s[end]], [y_s[end], y_s[end]],
-          color = :black, linestyle = :dash, label="bound")
+          color=:black, linestyle=:dash)
 
     return plt
 end
